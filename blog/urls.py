dafbd1e1610da2df468c8d8851b07909acc9abef
y.sui <pamelasui2016@gmail.com>
2019-11-13 23:39:22 +0800

    实现Blog标题到正文的跳转

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.blog_page)
+    path('', views.blog_page),
+    path('<int:blog_pk>', views.blog_text)
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from blog.models import Blog
 
 def blog_page(request):
     firstblog = Blog.objects
     return render(request, 'blog.html', {'firstblog': firstblog})
+
+def blog_text(request,blog_pk):
+    blog = get_object_or_404(Blog, pk=blog_pk)
+    return render(request, 'blog_text.html', {'blog':blog})
